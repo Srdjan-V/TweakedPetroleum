c92e51c96543ac7b17a4ec7c9e9354505f4608dd
Rongmario <rongmario@protonmail.com>
2022-08-08 18:27:07 +0100

    Correct fluid values for HEI plugin

--- src/main/java/srki2k/tweakedpetroleum/common/compat/hei/PumpjackCategory.java
+++ src/main/java/srki2k/tweakedpetroleum/common/compat/hei/PumpjackCategory.java
@@ -1,25 +1,14 @@
 package srki2k.tweakedpetroleum.common.compat.hei;
 
-
-import blusunrize.immersiveengineering.common.util.compat.jei.IEFluidTooltipCallback;
-import com.google.common.collect.Lists;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IGuiFluidStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
-import mezz.jei.api.gui.ITooltipCallback;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeCategory;
 import mezz.jei.util.Translator;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidStack;
 import srki2k.tweakedpetroleum.TweakedPetroleum;
-import srki2k.tweakedpetroleum.api.crafting.TweakedPumpjackHandler;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.OptionalInt;
 
 @SuppressWarnings("NullableProblems")
 public class PumpjackCategory implements IRecipeCategory<PumpjackWrapper> {
@@ -56,22 +45,12 @@ public class PumpjackCategory implements IRecipeCategory<PumpjackWrapper> {
 
         IGuiFluidStackGroup outputGuiFluids = recipeLayout.getFluidStacks();
 
-        TweakedPumpjackHandler.TweakedReservoirType reservoir = recipeWrapper.getReservoir();
-        Fluid reservoirFluid = recipeWrapper.getReservoirFluid();
+        outputGuiFluids.init(0, false, 12, 10, 16, 47, recipeWrapper.getMaxFluid(), false, null);
+        outputGuiFluids.set(0, recipeWrapper.getAverageFluid());
 
-        FluidStack averageFluid = recipeWrapper.getAverageFluid();
-        OptionalInt maxAverageFluid = recipeWrapper.getOptionalMaxAverageFluid();
-        OptionalInt maxFluidReplenishRate = recipeWrapper.getOptionalMaxFluidReplenishRate();
+        outputGuiFluids.init(1, false, 36, 10, 16, 47, recipeWrapper.getMaxFluidReplenishRate(), false, null);
+        outputGuiFluids.set(1, recipeWrapper.getAverageReplenishFluid());
 
-        if (maxAverageFluid.isPresent()) {
-            outputGuiFluids.init(0, false, 12, 10, 16, 47, maxAverageFluid.getAsInt(), false, null);
-            outputGuiFluids.set(0, averageFluid);
-        }
-
-        if (maxFluidReplenishRate.isPresent()) {
-            outputGuiFluids.init(1, false, 36, 10, 16, 47, maxFluidReplenishRate.getAsInt(), false, null);
-            outputGuiFluids.set(1, new FluidStack(reservoirFluid, reservoir.replenishRate));
-        }
     }
 
 }
